ff41d1c0a23bcd41f582eb32f69ee93d05a5b766
Andre Pires <andrecabralpires@gmail.com>
2018-07-16 10:57:43 +0100

    lecture

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
+
+require 'faker'
 # This is where you can create initial data for your app.
+
+#Create data
+
+
+
+
+puts 'Creating 100 fake restaurants...'
+100.times do
+  restaurant = Restaurant.new(
+    name:    Faker::GameOfThrones.character,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    rating:  rand(0..5)
+  )
+  restaurant.save!
+end
+puts 'Finished!'
